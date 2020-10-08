7de655f50d673ce8213c30eb1461db45d6760063
Nidhin <nidhin@trellisys.net>
2020-10-09 01:53:54 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.reemKufiTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
